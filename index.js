bdc6913c2c5e1893785ca41e3ddd2bdaef6098c7
Roman Land <roman.land@gmail.com>
2014-10-17 22:26:13 +0300

    adding mongodb integratino

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
+var mongo = require("mongodb").MongoClient;
 
+var mdb = null;
+
+mongo.connect('mongodb://127.0.0.1:27017/streets', function(err, db){
+	if (err) throw err;
+	mdb = db;
+	//var collection = db.collection('stories');
+	//collection.insert({"Test":true}, function(err, docs){
+	//	collection.count(function(err, count){
+	//		console.log("count",count);
+	//	})
+	//})
+})
 
 app.set('port', (process.env.PORT || 5555));
 
@@ -8,6 +21,13 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res){
 	console.log("sending streets image");
 	res.send("<html><body><h2>Streets Tel-Aviv</h2><img src='/public/streets.png'</img></body></html>");
+});
+
+app.get('/story', function(req,res){
+	mdb.collection('stories').count(function(err, count){
+		console.log("count",count);
+		res.send(count);
+	});
 });
 
 app.listen(app.get('port'), function() {
