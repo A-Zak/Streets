3fd2a318a3291ec7fd5ae9b66b18fb6009f921c1
Alex Zak <a@alexzak.me>
2014-10-17 17:27:33 +0300

    add header

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5555));
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res){
 	console.log("sending streets image");
-	res.send("<html><body><img src='/public/streets.png'</img></body></html>");
+	res.send("<html><body><h2>Streets Tel-Aviv</h2><img src='/public/streets.png'</img></body></html>");
 });
 
 app.listen(app.get('port'), function() {
